0174eaf9a4a8af2fa806d23a1de602a1e8724df2
DutraGames <gabrieldutraamarante@gmail.com>
2024-02-07 10:38:22 -0300

    refactor: struct App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import Logo from "./assets/Logo-NLW.svg";
+
 export const App = () => {
-  return <h1 className="text-3xl font-bold">Hello World</h1>;
+  return (
+    <div className="max-w-6xl mx-auto my-12 space-y-4">
+      <img src={Logo} alt="Logo NLW Expert" />
+      <form action="" className="w-full">
+        <input
+          type="text"
+          placeholder="Pesquisar uma nota..."
+          className="bg-transparent text-3xl font-semibold placeholder:text-slate-500 outline-none w-full -tracking-tight"
+        />
+      </form>
+
+      <div className="h-px w-full bg-slate-500"></div>
+    </div>
+  );
 };
