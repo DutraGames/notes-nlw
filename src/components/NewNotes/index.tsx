c36b1457f63909a4c8301c0e9bf3339254143ee9
DutraGames <gabrieldutraamarante@gmail.com>
2024-02-07 11:23:33 -0300

    chore: create component NewNote

---
+++ src/components/NewNotes/index.tsx
@@ -0,0 +1,10 @@
+export const NewNotes = () => {
+  return (
+    <div className="rounded-md bg-slate-700 p-5 space-y-3">
+      <span className="text-slate-200 text-sm font-medium">Adicionar Nota</span>
+      <p className="text-slate-400 leading-6 text-sm">
+        Grave uma nota em áudio que será convertida para texto automaticamente.
+      </p>
+    </div>
+  );
+};
