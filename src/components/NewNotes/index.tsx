5b724737ca406fc12881a1908588b47f8a4809ea
DutraGames <gabrieldutraamarante@gmail.com>
2024-02-08 12:40:46 -0300

    feat: add SpeechRecognition to recording notes

--- src/components/NewNotes/index.tsx
+++ src/components/NewNotes/index.tsx
@@ -10,6 +10,9 @@ export const NewNotes = ({ onAddNote }: NewNotesProps) => {
   const [shouldShowOnboarding, setShouldShowOnboarding] =
     useState<boolean>(true);
   const [content, setContent] = useState<string>("");
+  const [isRecording, setIsRecording] = useState<boolean>(false);
+
+  let recognition: SpeechRecognition | null;
 
   const handleShowEditor = () => {
     setShouldShowOnboarding(false);
@@ -24,10 +27,60 @@ export const NewNotes = ({ onAddNote }: NewNotesProps) => {
 
   const handleOnSave = (e: FormEvent) => {
     e.preventDefault();
+
+    if (content === "") {
+      return;
+    }
+
     toast.success("Nota adicionada com sucesso!");
     onAddNote(content);
     setContent("");
     setShouldShowOnboarding(true);
+  };
+
+  const handleStartRecording = () => {
+    const isSpeechRecognitionSupported =
+      "SpeechRecognition" in window || "webkitSpeechRecognition" in window;
+
+    if (!isSpeechRecognitionSupported) {
+      alert("Seu navegador não suporta o reconhecimento de fala");
+      return;
+    }
+
+    setIsRecording(true);
+    setShouldShowOnboarding(false);
+
+    const speechRecognitionApi =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+
+    recognition = new speechRecognitionApi();
+
+    recognition.lang = "pt-BR";
+    recognition.continuous = true;
+    recognition.maxAlternatives = 1;
+    recognition.interimResults = true;
+
+    recognition.onresult = (event) => {
+      const transcrition = Array.from(event.results).reduce((text, result) => {
+        return text.concat(result[0].transcript);
+      }, "");
+
+      setContent(transcrition);
+    };
+
+    recognition.onerror = (event) => {
+      console.error("Erro no reconhecimento de fala:", event.error);
+    };
+
+    recognition.start();
+  };
+
+  const handleStopRecording = () => {
+    setIsRecording(false);
+
+    if (recognition !== null) {
+      recognition.stop();
+    }
   };
 
   return (
@@ -56,7 +109,7 @@ export const NewNotes = ({ onAddNote }: NewNotesProps) => {
           <Dialog.Close className="absolute top-0 right-0 bg-slate-800 p-5">
             <span className="size-5 houver:text-slate-100">X</span>
           </Dialog.Close>
-          <form onSubmit={handleOnSave} className="flex flex-1 flex-col">
+          <form className="flex flex-1 flex-col">
             <div className="flex flex-1 flex-col gap-3 p-5">
               <span className="text-slate-300 text-sm font-medium">
                 Adicionar Nota
@@ -64,13 +117,18 @@ export const NewNotes = ({ onAddNote }: NewNotesProps) => {
               {shouldShowOnboarding ? (
                 <p className="text-slate-400 leading-6 text-sm">
                   Comece{" "}
-                  <button className="text-lime-400 font-medium hover:underline">
+                  <button
+                    className="text-lime-400 font-medium hover:underline"
+                    onClick={handleStartRecording}
+                    type="button"
+                  >
                     gravando uma nota
                   </button>{" "}
                   em áudio ou se preferir{" "}
                   <button
                     className="text-lime-400 font-medium hover:underline"
                     onClick={handleShowEditor}
+                    type="button"
                   >
                     utilize apenas texto
                   </button>
@@ -87,13 +145,26 @@ export const NewNotes = ({ onAddNote }: NewNotesProps) => {
               )}
             </div>
 
-            <button
-              type="submit"
-              className="w-full bg-lime-400 text-lime-950 py-4 text-center text-sm 
+            {!isRecording ? (
+              <button
+                type="button"
+                onClick={handleOnSave}
+                className="w-full bg-lime-400 text-lime-950 py-4 text-center text-sm 
             outline-none font-medium hover:bg-lime-500"
-            >
-              Salvar nota
-            </button>
+              >
+                Salvar nota
+              </button>
+            ) : (
+              <button
+                type="button"
+                onClick={handleStopRecording}
+                className="w-full bg-slate-900 text-slate-400 py-4 text-center text-sm 
+            outline-none font-medium hover:text-slate-100 flex justify-center items-center gap-2"
+              >
+                <div className="size-3 bg-red-500 rounded-full animate-pulse" />
+                Gravando! (clique p/ interromper)
+              </button>
+            )}
           </form>
         </Dialog.DialogContent>
       </Dialog.Portal>
